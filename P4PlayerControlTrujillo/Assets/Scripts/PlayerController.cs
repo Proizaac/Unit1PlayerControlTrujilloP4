5f0d80a0a26684ff0b1cfd501e1819396fd1cc30
Proizaac <762769@student.dusd.net>
2026-02-02 11:58:03 -0800

    created project
    added assets car moves and last left on the turning script

---
+++ P4PlayerControlTrujillo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float turnSpeed;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward   
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.right * Time.deltaTime * turnSpeed);
+    }
+}
